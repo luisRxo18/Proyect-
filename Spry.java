7ca728d99bb3c6292dde8a5ef16026a7c48b7c02
alwaysmile24 <vanne24_smile@hotmail.com>
2017-11-13 20:58:21 -0600

    Create Spry.java

---
+++ Spry.java
@@ -0,0 +1,32 @@
+package poo.videojuego;
+
+public class Spry implements poo.videojuego.Item{
+	//Atributos
+	private String nombre;
+	private int energiaAportada;
+	private int experienciaAportada;
+
+	//Constructor
+	public Spry(String nombre, int energiaAportada, int experienciaAportada){
+		this.nombre = nombre;
+		this.energiaAportada = energiaAportada;
+		this.experienciaAportada = experienciaAportada;
+	}
+
+	public Spry(String nombre){
+		this.nombre=nombre;
+		energiaAportada=50;
+		experienciaAportada=0;
+	}
+
+	//Metodos implementados
+	public  String getNombre(){
+		return nombre;
+	}
+	public int getEnergiaQueAporta(){
+		return energiaAportada;
+	}
+	public int getExperienciaQueAporta(){
+		return experienciaAportada;
+	}
+}
