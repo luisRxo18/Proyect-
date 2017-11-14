3a183762f945b2542ac88f93b2075195d33604ae
alwaysmile24 <vanne24_smile@hotmail.com>
2017-11-13 21:56:41 -0600

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,25 +1,34 @@
 package poo.videojuego;
 
+/** 
+Esta clase implementa la clase Item
+*/
 public class Item implements poo.videojuego.Item{
-	//Atributos
 	private String nombre;
 	private int energiaAportada;
 	private int experienciaAportada;
 
-	//Constructor
+	/** 
+	@param nombre es el nombre del Item
+	@param energiaAportada es el valor de la energia aportada
+	@param experienciaAportada es el valor de la experiencia aportada */
 	public Item(String nombre, int energiaAportada, int experienciaAportada){
 		this.nombre = nombre;
 		this.energiaAportada = energiaAportada;
 		this.experienciaAportada = experienciaAportada;
 	}
 
-	//Metodos implementados
+	// @return una cadena con el nombre del Item 
 	public  String getNombre(){
 		return nombre;
 	}
+	
+	// @return un entero con el valor de la energia que aporta
 	public int getEnergiaQueAporta(){
 		return energiaAportada;
 	}
+	
+	// @return un entero con el valor de la experiencia que aporta
 	public int getExperienciaQueAporta(){
 		return experienciaAportada;
 	}
