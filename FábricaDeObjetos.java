b3e5953cdba1aca9c7e3ad43ebb0b64564ba24aa
luisRxo18 <32626310+luisRxo18@users.noreply.github.com>
2017-11-13 20:35:39 -0600

    ya

--- FábricaDeObjetos.java
+++ FábricaDeObjetos.java
@@ -48,7 +48,16 @@ public  Collection<Item> construyeUnItemDeCadaTipo(){
   
   public  Item construyeItemPorNombre(String paramString);
   
-  public  Collection<Ataque> construyeUnAtaqueDeCadaClase();
+public  Collection<Ataque> construyeUnAtaqueDeCadaClase()	{
+	private Collection<Ataque> attacks = new  Collection<Ataque>();
+	Ataque golpe = new Ataque1("puño del dragon", 60,0,30);
+	Ataque patada = new Ataque1("patada de borracho", 30,0,50);
+	Ataque especial = new Ataque1("Gordificio", 100,0,90);
+	attacks.add(golpe);
+	attacks.add(patada);
+	attacks.add(especial);
+	return attacks;
+}
   
   public  Ataque construyeAtaquePorNombre(String paramString);
 }
