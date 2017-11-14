42a9ccc1eb58be28a725ab2a8779ac40dafbc170
alwaysmile24 <vanne24_smile@hotmail.com>
2017-11-13 20:34:38 -0600

    Update Partida.java

--- Partida.java
+++ Partida.java
@@ -6,9 +6,9 @@ public class Partida {
 		 Item up = new Item("potenciador",0,60);
 		 Item spry = new Item("Spry de salud",50,0);
 		 
-		 Ataque golpe = new Ataque1("puño del dragon", 60,0,30);
-		 Ataque patada = new Ataque1("patada de borracho", 30,0,50);
-		 Ataque especial = new Ataque1("Gordificio", 100,0,90);
+		 Ataque golpe = new Ataque("puño del dragon", 60,0,30);
+		 Ataque patada = new Ataque("patada de borracho", 30,0,50);
+		 Ataque especial = new Ataque("Gordificio", 100,0,90);
 
 
 
