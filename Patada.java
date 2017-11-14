f1099246ba70b3c06a6a22afe598fd0595fd19ee
alwaysmile24 <vanne24_smile@hotmail.com>
2017-11-13 20:52:17 -0600

    Create Patada.java

---
+++ Patada.java
@@ -0,0 +1,38 @@
+package poo.videojuego;
+
+public class Patada implements poo.videojuego.Ataque{
+	//Atributos
+	private int experienciaAportada;
+	private int danoCausado;
+	private int experienciaNecesaria;
+	private String nombre;
+
+	//Constructor
+	public Patada(String nombre,int experienciaNecesaria,int experienciaAportada,int danoCausado){
+		this.nombre=nombre;
+		this.experienciaNecesaria =experienciaNecesaria;
+		this.experienciaAportada=experienciaAportada;
+		this.danoCausado=danoCausado;
+	}
+
+	public Patada(String nombre){
+		this.nombre = nombre;
+		experienciaNecesaria=50;
+		experienciaAportada=0;
+		danoCausado=50;
+	}
+
+	//Metodos implementados:
+	public int getExperienciaQueAporta(){
+		return experienciaAportada;
+	}
+	public int getDanoQueCausa(){
+		return danoCausado;
+	}
+	public int getExperienciaNecesaria(){
+		return experienciaNecesaria;
+	}
+	public String getNombre(){
+		return nombre;
+	}
+}
