eb68dfbbe75b2cd3cbe7da3cdeb255046ee5fd86
luisRxo18 <32626310+luisRxo18@users.noreply.github.com>
2017-11-13 18:34:43 -0600

    partida

---
+++ Partida.java
@@ -0,0 +1,16 @@
+public class Partida {
+
+	public static void main() {
+
+		 Item hierbas = new Item("hierbas",20,30 );
+		 Item up = new Item("potenciador",0,60);
+		 Item spry = new Item("Spry de salud",50,0);
+		 
+		 Ataque golpe = new Ataque1("puño del dragon", 60,0,30);
+		 Ataque patada = new Ataque1("patada de borracho", 30,0,50);
+		 Ataque especial = new Ataque1("Gordificio", 100,0,90);
+
+
+
+	}
+}
