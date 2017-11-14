beafd09357ddbdc8523d8f213acba98107e14361
alwaysmile24 <vanne24_smile@hotmail.com>
2017-11-13 21:51:17 -0600

    Update Especial.java

--- Especial.java
+++ Especial.java
@@ -1,19 +1,28 @@
 package poo.videojuego;
 
+/**
+Esta es una clase llamada Especial que implementa Ataque 
+*/
 public class Especial implements poo.videojuego.Ataque{
-	//Atributos
 	private int experienciaAportada;
 	private int danoCausado;
 	private int experienciaNecesaria;
 	private String nombre;
 
-	//Constructor
+	/** 
+	@param nombre es el nombre del Ataque Especial
+	@param experienciaNecesaria es el valor de la experiencia necesaria
+	@param experienciaAportada es el valor de la experiencia aportada
+	@param danoCausado es el valor del dano que causara */
 	public Especial (String nombre,int experienciaNecesaria,int experienciaAportada,int danoCausado){
 		this.nombre=nombre;
 		this.experienciaNecesaria =experienciaNecesaria;
 		this.experienciaAportada=experienciaAportada;
 		this.danoCausado=danoCausado;
 	}
+	
+	/** Este constructor dara los valores a las variables 
+	param nombre es el nombre del Ataque Especial */
 	public Especial (String nombre){
 		this.nombre=nombre;
 		experienciaNecesaria = 100;
@@ -21,16 +30,22 @@ public class Especial implements poo.videojuego.Ataque{
 		danoCausado = 90;
 	}
 
-	//Metodos implementados:
+	// @return un entero con el valor de la experiencia aportada
 	public int getExpetuenciaQueAporta(){
 		return experienciaAportada;
 	}
+	
+	// @return un entero con el valor del dano causado
 	public int getDanoQueCausa(){
 		return danoCausado;
 	}
+	
+	// @return un entero con el valor de la experiencia necesaria
 	public int getExperienciaNecesaria(){
 		return experienciaNecesaria;
 	}
+	
+	// @return una cadena con el nombre del ataque
 	public String getNombre(){
 		return nombre;
 	}
