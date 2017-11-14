e02245fadf4430cceae67c6a73d5bc09a360e101
luisRxo18 <32626310+luisRxo18@users.noreply.github.com>
2017-11-13 18:45:49 -0600

    Asi?

--- FábricaDeObjetos.java
+++ FábricaDeObjetos.java
@@ -1,1 +1,20 @@
+package poo.videojuego;
+
+import java.util.Collection;
+
+public class Fabrica implements FabricaDeObjetos
+{
+  public abstract Personaje construyePersonajeDesdeCadena(String paramString);
+
+  
+  public abstract Collection<Personaje> cargaPesonajesDesdeArchivo(String paramString);
+  
+  public abstract Collection<Item> construyeUnItemDeCadaTipo();
+  
+  public abstract Item construyeItemPorNombre(String paramString);
+  
+  public abstract Collection<Ataque> construyeUnAtaqueDeCadaClase();
+  
+  public abstract Ataque construyeAtaquePorNombre(String paramString);
+}
 
