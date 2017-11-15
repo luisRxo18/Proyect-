606ab981cf02198536a5d5b3728bf45e9b3f9319
JulioCHCorona <yoy_gla@hotmail.com>
2017-11-14 22:08:52 -0600

    Update FábricaDeObjetos.java

--- FábricaDeObjetos.java
+++ FábricaDeObjetos.java
@@ -1,44 +1,54 @@
 package poo.videojuego;
-import java.util.Collection;
+import java.io.*;
+import java.util.*;
+import java.util.StringTokenizer;
 
 public class Fabrica implements FabricaDeObjetos
 {
+	int vid,exp;
   public  Personaje construyePersonajeDesdeCadena(String paramString)
   {
-	private String[] arreglo = new String[3];
+	String arreglo[] = new String[3];
+	int x=0;
   	StringTokenizer personaje = new StringTokenizer(paramString,"|");
 	Personaje pers;
-  	while (personaje.hasMoreTokens){
-  		arreglo[x] = pers.nextToken();
+  	while (personaje.hasMoreTokens()){
+  		arreglo[x] = personaje.nextToken();
+  		x++;
   	}
-	per = new (arreglo[0],Integer.parseInt(arreglo[1]),Integer.parseInt(arreglos[2]));
-	return per;
+  	vid = Integer.parseInt(arreglo[1]);
+  	exp = Integer.parseInt(arreglo[2]);
+	pers = new Hombre(arreglo[0],vid,exp);
+	return pers;
   }
 
   
   public  Collection<Personaje> cargaPesonajesDesdeArchivo(String paramString){
-	private int x = 0;
-	private Collection<Personaje> personajes = new  Collection<Personaje>();
+	 int x = 0;
+	 String datos;
+	 Collection<Personaje> personajes = new ArrayList<Personaje>();
 	try {
-     	File archivo = new File("personajes.txt");
-      	BufferedReader br;
+    File archivo = new File("personajes.txt");
+    BufferedReader br;
 	FileReader fr = new FileReader("personajes.txt");
 	br = new BufferedReader(fr);
-	while ((datos = br.readLine())!= null){
+	while ((datos = br.readLine()) != null){
 	   	personajes.add(construyePersonajeDesdeCadena(datos));
 	}
+	br.close();    
+    fr.close();
 	}
 	catch(IOException e){
         System.out.println("Error");
-    	}
+    }
 	return personajes;
   }
   
 public  Collection<Item> construyeUnItemDeCadaTipo(){
-	private Collection<Item> items = new  Collection<Item>();
-	Hierbas hierb = new Item("hierbas rojas",20,30 );
-	Up upp = new Item("potenciador",0,60);
-	Spry spr = new Item("Spry de salud",50,0);
+	Collection<Item> items = new  ArrayList<Item>();
+	Hierbas hierb = new Hierbas("hierbas rojas",20,30 );
+	Up upp = new Up("potenciador",0,60);
+	Spry spr = new Spry("Spry de salud",50,0);
 	items.add(hierb);
 	items.add(upp);
 	items.add(spr);
@@ -52,7 +62,7 @@ public  Collection<Item> construyeUnItemDeCadaTipo(){
   }
   
 public  Collection<Ataque> construyeUnAtaqueDeCadaClase()	{
-	private Collection<Ataque> attacks = new  Collection<Ataque>();
+	Collection<Ataque> attacks = new  ArrayList<Ataque>();
 	Golpe golpe = new Golpe("puño del dragon", 60,0,30);
 	Patada patada = new Patada("patada de borracho", 30,0,50);
 	Especial especial = new Especial("Gordificio", 100,0,90);
@@ -63,7 +73,7 @@ public  Collection<Ataque> construyeUnAtaqueDeCadaClase()	{
 }
   //pendiente 
   public  Ataque construyeAtaquePorNombre(String paramString){
-	Hierbas herbal = new Hierbas(paramString);
-	return herbal; 
+	Especial attack = new Especial(paramString);
+	return attack; 
 }
 }
