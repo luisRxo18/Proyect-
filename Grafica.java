3ba13cb585187041c641adbc1d8e925728afd6b5
luisRxo18 <32626310+luisRxo18@users.noreply.github.com>
2017-11-14 21:46:01 -0600

    PARTIDAUI

---
+++ Grafica.java
@@ -0,0 +1,38 @@
+package poo.videojuego;
+
+public class Grafica implements PartidaUI {
+
+
+
+	public void muestraEstadoPartida(){
+
+	}
+	public void configurado(){
+
+	}
+	public void iniciaPartida(){
+
+	}
+	public void iniciaRonda(){
+
+	}
+	public void iniciaTurno(){
+
+	}
+	public void terminaTurno(){
+
+	}
+	public void terminaPartida() {
+
+	}
+	public void despliegaMensaje(String mensaje){
+
+	}
+	public void despliegaError(String error){
+
+
+	}
+	public void limpiamensajes(){
+		
+	}
+}
