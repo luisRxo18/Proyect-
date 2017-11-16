b1359109d3b65807160d94d91c9505bb78c71165
ignacioambia <ignacio.ambia@gmail.com>
2017-11-15 21:11:36 -0600

    Update Especial.java

--- Especial.java
+++ Especial.java
@@ -1,5 +1,7 @@
 package poo.videojuego;
 
+/** Clase que impletmenta de Ataque.*/
+
 public class Especial implements poo.videojuego.Ataque{
 	//Atributos
 	private int experienciaAportada;
@@ -8,12 +10,18 @@ public class Especial implements poo.videojuego.Ataque{
 	private static String nombre="Especial";
 
 	//Constructor
+	/**Constructor
+	@param nombre Es el nombre de el ataque
+	*/
 	public Especial (String nombre,int experienciaNecesaria,int experienciaAportada,int danoCausado){
 		this.nombre=nombre;
 		this.experienciaNecesaria =experienciaNecesaria;
 		this.experienciaAportada=experienciaAportada;
 		this.danoCausado=danoCausado;
 	}
+	/**Constructor
+	@param nombre Recibimos nombre en forma de String
+	*/
 	public Especial (String nombre){
 		this.nombre=nombre;
 		experienciaNecesaria = 100;
