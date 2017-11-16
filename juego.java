21549fdfbe6bd26b3098ef9f0e9305412899963c
luisRxo18 <32626310+luisRxo18@users.noreply.github.com>
2017-11-15 18:58:01 -0600

    nel

--- juego.java
+++ juego.java
@@ -1,42 +1,51 @@
 package poo.videojuego;
 
-public class juego implements poo.videojuego.Partida {
+public class juego implements poo.videojuego.ui.Partida {
 private FabricaDeObjetos fabrica;
 private String archivo;
 private PartidaUI ui;
 private int totalRondas;
-private int itemsIniciales;
-	public void configura(FabricaDeObjetos fabrica, String archivo, PartidaUI ui, int totalRondas, int itemsIniciales){
+private int itmesIniciales=3;
+private Collection<Item> items;
+private Collection<Ataque> ataques;
+private Item aux;
+private Collection<Personajes> pers;
+	public void configura(FabricaDeObjetos fabrica, String archivo, PartidaUI ui, int totalRondas, int itmesIniciales){
 			this.fabrica = fabrica;
 			this.archivo = archivo;
 			this.ui=ui;
 			this.totalRondas=totalRondas;
-			this.itemsIniciales=itemsIniciales;
+			this.itmesIniciales=itmesIniciales;
 	} 
 	public void inicia(){
-
-
+	fabrica.cargaPersonajesDesdeArchivo();
+	this.totalRondas=3;
+	this.itemsIniciales=3;
+	items = new ArrayList<Item>();
+	items = fabrica.construyeUnItemDeCadaTipo();
+	ataques = fabrica.construyeUnAtaqueDeCadaClase();
 	}
+	
 	public Collection<Item> getItemsDisponibles(){
-		return null; 
-
+		
+		return  items;
 	}
 	public Collection<ataque> getAtaquesDisponibles(){
-		return null; 
+		return ataques; 
 
 	}
 	public Collection<Personajes> getPersonajes() {
-		return null;
+		return pers;
 	}
 	public Personaje getPersonajeEnTurno(){
 		return null;
 	}
 	public int RondaActual() {
-		return null; 
+		return ; 
 
 	}
 	public int getTotalRondas() {
-		return null;
+		return totalRondas;
 	}
 	public void itemUsado(Item item){
 		
