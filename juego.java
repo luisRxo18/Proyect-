7ff7b64c9d118e78581dfa197d8e9eb72ab12c04
JulioCHCorona <yoy_gla@hotmail.com>
2017-11-15 21:43:33 -0600

    Update juego.java

--- juego.java
+++ juego.java
@@ -6,15 +6,16 @@ import poo.videojuego.Fabrica;
 
 public class Juego implements poo.videojuego.ui.Partida {
 private FabricaDeObjetos fabrica;
+private Accion accion;
 private String archivo;
 private PartidaUI ui;
 private int totalRondas;
 private int itemsIniciales=3;
-private Collection<Item> items;
-private Collection<Ataque> ataques;
+private Collection<Item> items = new ArrayList<Item>();
+private Collection<Ataque> ataques = new ArrayList<Ataque>();
 private Item aux;
 private int rondaActual;
-private Collection<Personaje> pers;
+private Collection<Personaje> pers = new ArrayList<Personaje>();
 private Personaje persoajeActual;
 
 	public void configura(FabricaDeObjetos fabrica, String archivo, PartidaUI ui, int totalRondas, int itmesIniciales){
@@ -23,15 +24,24 @@ private Personaje persoajeActual;
 			this.ui=ui;
 			this.totalRondas=totalRondas;
 			this.itemsIniciales=itmesIniciales;
+			pers = this.fabrica.cargaPesonajesDesdeArchivo("personajes.txt");
+			ataques = this.fabrica.construyeUnAtaqueDeCadaClase();
+			items =this.fabrica.construyeUnItemDeCadaTipo();
+
 	} 
 	public void inicia(){
-        
-	pers = fabrica.cargaPesonajesDesdeArchivo(archivo);
-	this.totalRondas=3;
-	this.itemsIniciales=3;
-	items = new ArrayList<Item>();
-	items = fabrica.construyeUnItemDeCadaTipo();
-	ataques = fabrica.construyeUnAtaqueDeCadaClase();
+	ui.iniciaPartida();
+	for (int i=0; i<totalRondas; i++){
+		ui.iniciaRonda();
+		rondaActual = rondaActual-1;
+		for (int j=0; j<pers.size(); j++){
+			ui.limpiaMensajes();
+			ui.iniciaTurno();
+			accion.ejecuta();
+			ui.terminaTurno();
+			ui.limpiaMensajes();
+		}
+	}
 	}
 	
 	public Collection<Item> getItemsDisponibles(){
@@ -40,7 +50,6 @@ private Personaje persoajeActual;
 	}
 	public Collection<Ataque> getAtaquesDisponibles(){
 		return ataques; 
-
 	}
 	public Collection<Personaje> getPersonajes() {
 		return pers;
@@ -62,7 +71,7 @@ private Personaje persoajeActual;
 	}
     public void ejecutaAccion(Accion a)
     {
-        System.out.println("accion \n");
+        
     }
 
 }
