9ded6ec6410a27148785a6028b6e45056c3d4f33
luisRxo18 <32626310+luisRxo18@users.noreply.github.com>
2017-11-15 20:12:57 -0600

    update

--- juego.java
+++ juego.java
@@ -1,24 +1,32 @@
 package poo.videojuego;
+import poo.videojuego.ui.PartidaUI;
+import poo.videojuego.ui.Accion;
+import java.util.*;
+import poo.videojuego.Fabrica;
 
 public class juego implements poo.videojuego.ui.Partida {
 private FabricaDeObjetos fabrica;
 private String archivo;
 private PartidaUI ui;
 private int totalRondas;
-private int itmesIniciales=3;
+private int itemsIniciales=3;
 private Collection<Item> items;
 private Collection<Ataque> ataques;
 private Item aux;
-private Collection<Personajes> pers;
+private int rondaActual;
+private Collection<Personaje> pers;
+private Personaje persoajeActual;
+
 	public void configura(FabricaDeObjetos fabrica, String archivo, PartidaUI ui, int totalRondas, int itmesIniciales){
 			this.fabrica = fabrica;
 			this.archivo = archivo;
 			this.ui=ui;
 			this.totalRondas=totalRondas;
-			this.itmesIniciales=itmesIniciales;
+			this.itemsIniciales=itmesIniciales;
 	} 
 	public void inicia(){
-	fabrica.cargaPersonajesDesdeArchivo();
+        
+	fabrica.cargaPersonajesDesdeArchivo("hola");
 	this.totalRondas=3;
 	this.itemsIniciales=3;
 	items = new ArrayList<Item>();
@@ -30,25 +38,31 @@ private Collection<Personajes> pers;
 		
 		return  items;
 	}
-	public Collection<ataque> getAtaquesDisponibles(){
+	public Collection<Ataque> getAtaquesDisponibles(){
 		return ataques; 
 
 	}
-	public Collection<Personajes> getPersonajes() {
+	public Collection<Personaje> getPersonajes() {
 		return pers;
 	}
 	public Personaje getPersonajeEnTurno(){
 		return null;
 	}
-	public int RondaActual() {
-		return ; 
+	public int getRondaActual() {
+		return rondaActual;
 
 	}
 	public int getTotalRondas() {
 		return totalRondas;
 	}
 	public void itemUsado(Item item){
+        persoajeActual.usa(item);
+        items.remove(item);
 		
 	}
+    public void ejecutaAccion(Accion a)
+    {
+        System.out.println("accion \n");
+    }
 
 }
